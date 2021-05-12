4728cffafcffdadd892e057b19c01a2853f5ff73
stackreactor.info <davenew13@gmail.com>
2021-05-12 12:31:34 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import './App.css';
 
+function WelcomeBack({name="valued customer", adjective="nice"}) {
+  return <p>Welcome back, {adjective} {name}!</p>;
+}
+
+function EncouragingWords({name}){
+  return <p>Believe you can, {name}, and you're halfway there!</p>;
+}
+
+function DailyTip(){
+  return <p>Highlight a whole paragraph by triple-clicking on it</p>;
+}
+
+function Header(){
+  return <h1>App Header</h1>;
+}
+
+function Nav(){
+  return <h2>Navigation</h2>;
+}
+
+function Component1(){
+  return <p>Component 1 Content</p>
+}
+
+function Component2(){
+  return <p>Component 2 Content</p>
+}
+function Component3(){
+  return <p>Component 3 Content</p>
+}
+
+function Footer(){
+  return <h3>Footer</h3>
+}
 function App() {
-  return <h1>Hello World!</h1>
+  return <>
+    <WelcomeBack name="Mina" adjective="cute"/>
+    <WelcomeBack name="Nick" adjective="handsome"/>
+    <WelcomeBack />
+    <EncouragingWords name="Mina"/>
+    <DailyTip/>
+    <Header/>
+    <Nav/>
+    <Component1/>
+    <Component2/>
+    <Component3/>
+    <Footer/>
+  </>;
 }
 
 export default App;
